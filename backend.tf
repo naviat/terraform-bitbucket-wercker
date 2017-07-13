12772b6a03feefccdcdc5ba9aa04855a72630959
naviat <haidv.ict@gmail.com>
2017-07-13 15:25:27 +0700

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,9 +2,6 @@ terraform {
   backend "s3" {
     bucket = "haidv.terraform.state"
     key    = "terraform.tfstate"
+    region = "ap-southeast-1"
   }
  }
-
-provider "aws" {
-  region = "ap-southeast-1"
-}
